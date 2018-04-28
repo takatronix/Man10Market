8700beb07b6fda44bc4ea535b08ec65fd8d95756
takatronix <takatronix@gmail.com>
2018-04-29 05:14:48 +0900

    アイテムの登録と価格取得

--- src/main/java/red/man10/MarketCommand.java
+++ src/main/java/red/man10/MarketCommand.java
@@ -19,6 +19,55 @@ public class MarketCommand implements CommandExecutor {
 
         Player p = (Player) sender;
 
+        p.sendMessage("length="+args.length);
+
+        if(args.length == 0){
+            showHelp(p);
+            return false;
+        }
+
+
+
+        String command = args[0];
+
+
+        ////////////////
+        //    登録
+        if(command.equals("register")){
+            if(args.length != 4){
+                p.sendMessage("§c§l/mm register 1)登録名称 2)初期金額 3)ティック(値動き幅) - 手にもったアイテムをマーケットに登録する");
+                return false;
+            }
+            plugin.registerItem(p,args[1], Double.parseDouble(args[2]),Double.parseDouble(args[3]));
+            return true;
+        }
+
+        ////////////////
+        //    リスト
+        if(command.equals("list")){
+            plugin.showList(p);
+            return true;
+        }
+
+        //////////////////////////
+        //      売り注文
+        if(command.equals("price")){
+            plugin.showPrice(p);
+            return true;
+        }
+        //////////////////////////
+        //      売り注文
+        if(command.equals("ordersell")){
+            if(args.length != 3){
+                p.sendMessage("§2§l/mm ordersell [一つあたりの金額] [個数] -  指定した金額で売り注文を出す");
+                return false;
+            }
+            return plugin.orderSell(p,Double.parseDouble(args[2]), Integer.parseInt(args[3]));
+        }
+
+
+
+
         this.showHelp(p);
         return true;
     }
@@ -27,20 +76,24 @@ public class MarketCommand implements CommandExecutor {
         p.sendMessage("§e============== §d●§f●§a●§e Man10 Market §d●§f●§a● §e===============");
         p.sendMessage("§e  by takatronix http://man10.red");
         p.sendMessage("§c* 赤いコマンドは管理者用です");
+
+
+        p.sendMessage("§2§l/mm list - 登録アイテムリストと価格を表示する");
+        p.sendMessage("§2§l/mm price (id/key) - (id or Key/手に持ったアイテム)の金額を表示する");
+
         p.sendMessage("/mm sellall - 手にもったアイテムすべてを市場価格で販売する");
 
-        p.sendMessage("/mm price [id/this] - (id/手に持ったアイテム)の金額を表示する");
         p.sendMessage("/mm order (user) 注文を表示する");
         p.sendMessage("/mm cancel [order_id] 注文をキャンセルする");
         p.sendMessage("/mm cancellall (userid) 全ての注文をキャンセルする");
 
         p.sendMessage("/mm orderbuy [id/this] [一つあたりの金額] [個数]- 指定した金額で買い注文を出す");
-        p.sendMessage("/mm ordersell [id/this] [一つあたりの金額] [個数] -  指定した金額で売り注文を出す");
+        p.sendMessage("§2§l/mm ordersell [一つあたりの金額] [個数] -  指定した金額で売り注文を出す");
         p.sendMessage("/mm marketbuy [id/this] [個数] - 成り行き注文（市場価格で購入)");
         p.sendMessage("/mm marketsell [id/this] [個数] - 成り行き注文（市場価格で売り)");
         p.sendMessage("§c--------------------------------");
-        p.sendMessage("§c*/mm register 初期金額 ティック(値動き幅) - 手にもったアイテムをマーケットに登録する");
-        p.sendMessage("§c*/mm unregister - 手にもったアイテムをマーケットから削除する");
+        p.sendMessage("§c§l/mm register 1)登録名称 2)初期金額 3)ティック(値動き幅) - 手にもったアイテムをマーケットに登録する");
+        p.sendMessage("§c/mm unregister - 手にもったアイテムをマーケットから削除する");
     }
     void showTable(CommandSender p){
 
--- src/main/java/red/man10/MarketPlugin.java
+++ src/main/java/red/man10/MarketPlugin.java
@@ -1,14 +1,109 @@
 package red.man10;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 
 public final class MarketPlugin extends JavaPlugin implements Listener {
 
 
-    String  prefix = "§f§l[Man10Market]";
+    String  prefix = "§f§l[§2§lm§e§lMarket§f§l]";
+    VaultManager vault = null;
+    MarketData data = null;
 
+
+
+    // アイテムの値段を表示
+    public boolean showPrice(Player p){
+        ItemStack item = p.getInventory().getItemInMainHand();
+
+        MarketData.PriceResult ret = data.getItemPrice(p,item);
+
+        if(ret.result == true){
+            double st = ret.price * 64;
+            showMessage(p,"現在価格:$" + data.getPriceString(ret.price) +"/個 $"+  data.getPriceString(st)+"/1Stack");
+        }else{
+            showError(p,"データ取得失敗");
+        }
+
+        return ret.result;
+    }
+
+    ///  売り注文を出す
+    public boolean orderSell(Player p,double price,int count){
+        ItemStack item = p.getInventory().getItemInMainHand();
+        if(item.getAmount() < count){
+            showError(p,"アイテムを"+count+"個もっていません");
+            return false;
+        }
+
+        return true;
+    }
+
+
+    //  アイテムリスト表示
+    public void showList(Player p){
+        data.showItemList(p);
+    }
+
+
+    // アイテム登録
+    public void registerItem(Player p,String key,double price,double tick){
+        p.sendMessage("アイテム登録");
+
+        ItemStack item = p.getInventory().getItemInMainHand();
+        if(item.getAmount() != 1){
+            p.sendMessage("§4§lマーケットに登録するアイテムを手に一つもってコマンドを実行してください");
+            return;
+        }
+
+
+        if(data.registerItem(p.getUniqueId(),item,key,price,tick)){
+           showMessage(p,"マーケットにアイテムを登録しました");
+        }else{
+            showError(p,"登録に失敗しました");
+        }
+
+    }
+
+    void loadConfig(){
+        data = new MarketData(this);
+        if(data == null){
+            if(data == null) {
+                Bukkit.getServer().broadcastMessage("DB接続エラー");
+                return;
+            }
+        }
+    }
+
+
+
+
+
+    //      ログメッセージ
+    void log(String text){
+        getLogger().info(text);
+    }
+    //     サーバーメッセージ
+    void serverMessage(String text){
+        Bukkit.getServer().broadcastMessage(prefix +  text);
+    }
+    //      プレイヤーメッセージ
+    void showMessage(Player p,String text){
+        p.sendMessage(prefix + text);
+    }
+
+    void showError(Player p,String text){
+        p.sendMessage("§4§lエラー:" + text);
+    }
 
     @Override
     public void onEnable() {
@@ -16,6 +111,9 @@ public final class MarketPlugin extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents (this,this);
         getCommand("mm").setExecutor(new MarketCommand(this));
 
+        vault = new VaultManager(this);
+        this.saveDefaultConfig();
+        this.loadConfig();
 
         Bukkit.getServer().broadcastMessage(prefix+"Started");
     }
@@ -24,4 +122,8 @@ public final class MarketPlugin extends JavaPlugin implements Listener {
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+
+
+
 }
