64e39dcf3d7812408cc6cc698e2d3f936486c1ed
takatronix <takatronix@gmail.com>
2018-05-02 13:57:42 +0900

    commit テスト3

--- src/main/java/red/man10/MarketPlugin.java
+++ src/main/java/red/man10/MarketPlugin.java
@@ -20,7 +20,7 @@ import java.util.Date;
 public final class MarketPlugin extends JavaPlugin implements Listener {
 
     //Mr_IK test_push
-    // takatronix push test2
+    // takatronix push test3
     String  prefix = "§f§l[§2§lm§e§lMarket§f§l]";
 
     MarketData data = null;
