36c9264a92f548234021a435719ed4a40df830ec
Chiharu-Hagihara <chiharuhagihara803@gmail.com>
2021-02-11 21:23:07 +0900

    fix: mce tick command error

--- src/main/java/red/man10/MarketCommand.java
+++ src/main/java/red/man10/MarketCommand.java
@@ -238,7 +238,12 @@ public class MarketCommand implements CommandExecutor {
                 return false;
             }
 
-            plugin.setTick(p,args[1], Double.parseDouble(args[2]));
+
+            try {
+                plugin.setTick(p, args[1], Double.parseDouble(args[2]));
+            }catch(NumberFormatException e) {
+                p.sendMessage("§4§lティックを数字にしてください。");
+            }
             return true;
         }
 
